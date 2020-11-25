2c42ba8e7c7174c22a12a96e3a6e30eb0222b083
massdo <dorian_massoulier@hotmail.com>
2020-11-25 11:57:33 +0100

    modif token

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ echo "\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-token=MAPBOX_KEY\n\
+token='pk.eyJ1IjoibWFzc2RvIiwiYSI6ImNraDR5MnBnNDBkZTIyeW85cmh3b3N3cXUifQ.AKd071Qk75Iw2UaNtNN_2Q'\n\
 \n\
 " > ~/.streamlit/config.toml
